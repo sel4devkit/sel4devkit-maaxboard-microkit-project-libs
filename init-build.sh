745f46f14a0714f5778b8d5a4908791da89f0d13
dstorer-cap <daniel.storer@capgemini.com>
2024-05-21 11:01:06 +0100

    Updated build script

--- init-build.sh
+++ init-build.sh
@@ -54,10 +54,10 @@ if [ "$user_input1" = "yes" ]; then
     ../scripts/do-aarch64-configure-nocrt -Dprefix=${PWD}/../../picolibc_build
     sudo ninja 
     sudo ninja install
+    cd ../../microkit
 fi
 
 # Build application 
-cd ../../microkit
 rm -rf example/maaxboard/$MICROKIT_APP/build
 mkdir example/maaxboard/$MICROKIT_APP/build
 rm -rf example/maaxboard/$MICROKIT_APP/example-build
