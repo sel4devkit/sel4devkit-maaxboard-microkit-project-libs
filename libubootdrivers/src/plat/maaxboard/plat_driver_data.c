b2f25ffcf2d115b7f0d4a1a32510e45d0a5b68a8
dstorer-cap <daniel.storer@capgemini.com>
2024-05-21 10:59:00 +0100

    Removed ethernet references

--- libubootdrivers/src/plat/maaxboard/plat_driver_data.c
+++ libubootdrivers/src/plat/maaxboard/plat_driver_data.c
@@ -29,7 +29,7 @@ void initialise_driver_data(void) {
     driver_data.uclass_driver_array[11] = _u_boot_uclass_driver__mmc;
     driver_data.uclass_driver_array[12] = _u_boot_uclass_driver__pinconfig;
     driver_data.uclass_driver_array[13] = _u_boot_uclass_driver__pinctrl;
-    driver_data.uclass_driver_array[14] = _u_boot_uclass_driver__ethernet;
+    // driver_data.uclass_driver_array[14] = _u_boot_uclass_driver__ethernet;
     driver_data.uclass_driver_array[15] = _u_boot_uclass_driver__led;
     driver_data.uclass_driver_array[16] = _u_boot_uclass_driver__gpio;
     driver_data.uclass_driver_array[17] = _u_boot_uclass_driver__i2c;
@@ -52,7 +52,7 @@ void initialise_driver_data(void) {
     driver_data.driver_array[12] = _u_boot_driver__fsl_esdhc;
     driver_data.driver_array[13] = _u_boot_driver__pinconfig_generic;
     driver_data.driver_array[14] = _u_boot_driver__imx8mq_pinctrl;
-    driver_data.driver_array[15] = _u_boot_driver__fecmxc_gem;
+    // driver_data.driver_array[15] = _u_boot_driver__fecmxc_gem;
     driver_data.driver_array[16] = _u_boot_driver__led_gpio_wrap;
     driver_data.driver_array[17] = _u_boot_driver__led_gpio;
     driver_data.driver_array[18] = _u_boot_driver__gpio_mxc;
@@ -102,9 +102,9 @@ void initialise_driver_data(void) {
     driver_data.cmd_array[17] = _u_boot_cmd__ext4write;
     driver_data.cmd_array[18] = _u_boot_cmd__part;
     driver_data.cmd_array[19] = _u_boot_cmd__clocks;
-    driver_data.cmd_array[20] = _u_boot_cmd__net;
-    driver_data.cmd_array[21] = _u_boot_cmd__dns;
-    driver_data.cmd_array[22] = _u_boot_cmd__ping;
+    // driver_data.cmd_array[20] = _u_boot_cmd__net;
+    // driver_data.cmd_array[21] = _u_boot_cmd__dns;
+    // driver_data.cmd_array[22] = _u_boot_cmd__ping;
     driver_data.cmd_array[23] = _u_boot_cmd__led;
     driver_data.cmd_array[24] = _u_boot_cmd__gpio;
     driver_data.cmd_array[25] = _u_boot_cmd__i2c;
@@ -112,12 +112,12 @@ void initialise_driver_data(void) {
     driver_data.cmd_array[27] = _u_boot_cmd__clk;
     driver_data.cmd_array[28] = _u_boot_cmd__pinmux;
 
-    driver_data.env_clbk_array[0] = _u_boot_env_clbk__ethaddr;
-    driver_data.env_clbk_array[1] = _u_boot_env_clbk__ipaddr;
-    driver_data.env_clbk_array[2] = _u_boot_env_clbk__gatewayip;
-    driver_data.env_clbk_array[3] = _u_boot_env_clbk__netmask;
-    driver_data.env_clbk_array[4] = _u_boot_env_clbk__serverip;
-    driver_data.env_clbk_array[5] = _u_boot_env_clbk__nvlan;
-    driver_data.env_clbk_array[6] = _u_boot_env_clbk__vlan;
-    driver_data.env_clbk_array[7] = _u_boot_env_clbk__dnsip;
+    // driver_data.env_clbk_array[0] = _u_boot_env_clbk__ethaddr;
+    // driver_data.env_clbk_array[1] = _u_boot_env_clbk__ipaddr;
+    // driver_data.env_clbk_array[2] = _u_boot_env_clbk__gatewayip;
+    // driver_data.env_clbk_array[3] = _u_boot_env_clbk__netmask;
+    // driver_data.env_clbk_array[4] = _u_boot_env_clbk__serverip;
+    // driver_data.env_clbk_array[5] = _u_boot_env_clbk__nvlan;
+    // driver_data.env_clbk_array[6] = _u_boot_env_clbk__vlan;
+    // driver_data.env_clbk_array[7] = _u_boot_env_clbk__dnsip;
 }
