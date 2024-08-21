4e0cfc7af3dff079a55ee0140516771e4b42eb5a
dstorer-cap <daniel.storer@capgemini.com>
2024-08-21 21:42:59 +0100

    Updated paths in build script

--- init-build.sh
+++ init-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Microkit path
-library_path=${PWD}
+# Project libs path
+PROJECT_LIBS_DIR=${PWD}
 
 # Initialize variables to store the values of the command-line options
 MICROKIT_APP=""
@@ -38,6 +38,9 @@ fi
 echo "MICROKIT_APP: $MICROKIT_APP"
 echo "PLATFORM: $PLATFORM"
 
+# Project Path
+PROJECT_DIR="$PROJECT_LIBS_DIR/../examples/maaxboard/$MICROKIT_APP"
+
 # Build picolibc
 echo "Do you want to build picolibc? (yes/no)"
 read user_input1
@@ -54,15 +57,15 @@ if [ "$user_input1" = "yes" ]; then
     ../scripts/do-aarch64-configure-nocrt -Dprefix=${PWD}/../../picolibc_build
     sudo ninja 
     sudo ninja install
-    cd ../../
 fi
 
 # Build application 
-rm -rf examples/maaxboard/$MICROKIT_APP/build
-mkdir examples/maaxboard/$MICROKIT_APP/build
-rm -rf examples/maaxboard/$MICROKIT_APP/example-build
-mkdir examples/maaxboard/$MICROKIT_APP/example-build
-cd examples/maaxboard/$MICROKIT_APP/build
-cmake -DMICROKIT_APP=$MICROKIT_APP -DPLATFORM=$PLATFORM $library_path
+cd $PROJECT_DIR
+rm -rf $PROJECT_DIR/build
+mkdir $PROJECT_DIR/build
+rm -rf $PROJECT_DIR/example-build
+mkdir $PROJECT_DIR/example-build
+cd $PROJECT_DIR/build
+cmake -DMICROKIT_APP=$MICROKIT_APP -DPLATFORM=$PLATFORM $PROJECT_LIBS_DIR
 make 
-echo "Built image is here: ${library_path}/example/${PLATFORM}/${MICROKIT_APP}/example-build/sel4_image.img" 
+echo "Built image is here: $PROJECT_DIR/example-build/sel4_image.img" 
